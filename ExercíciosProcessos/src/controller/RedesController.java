715939436a06ae2617c1f41f8e109dd96802ef74
roney <roney@roney-RF511>
2019-08-26 00:33:16 -0300

    Added linux compatibility

--- ExercíciosProcessos/src/controller/RedesController.java
+++ ExercíciosProcessos/src/controller/RedesController.java
@@ -6,85 +6,85 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class RedesController {
-	
+
 	public String OperationalSystem() {
 		String os = System.getProperty("os.name");
 		return os;
 	}
-	
+
 	public String callProcess(String process) {
-		String message="";
+		String message = "";
 		String adapter = "";
 		String ip = "";
-    	try {
+		try {
 			Process p = Runtime.getRuntime().exec(process);
 			InputStream fluxo = p.getInputStream();
 			InputStreamReader leitor = new InputStreamReader(fluxo);
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
-			while(linha != null ) {
-				
-				if(linha.contains("Adaptador")||linha.contains("en")||linha.contains("en")||linha.contains("wl")) {
-					if(linha.contains("en")||linha.contains("en")||linha.contains("wl")) {
-						adapter = linha.substring(0, (linha.indexOf(":")-1 	));
-					}else {
-					adapter = linha;
+			while (linha != null) {
+
+				if ((linha.contains("Adaptador") || linha.contains("et") || linha.contains("en") || linha.contains("wl")
+						|| linha.contains("lo"))
+						&& (!linha.contains("ether") && !linha.contains("ets") && !linha.contains("net")
+								&& !linha.contains("txqueuelen"))) {
+					if ((linha.contains("et") || linha.contains("en") || linha.contains("wl") || linha.contains("lo"))
+							&& (!linha.contains("ether") && !linha.contains("ets") && !linha.contains("net")
+									&& !linha.contains("txqueuelen"))) {
+						int iend = linha.indexOf(":");
+						adapter = linha.substring(0, iend).trim();
+					} else {
+						adapter = linha.trim();
 					}
 				}
-				if(linha.contains("IPv4")||linha.contains("inet ")) {
-					if(linha.contains("inet ")) {
-						adapter = linha.substring(0, (linha.lastIndexOf("netmask")));
-					}else{
-						ip = linha;
+				if (linha.contains("IPv4") || linha.contains("inet ")) {
+					if (linha.contains("inet ")) {
+						ip = linha.substring(0, (linha.lastIndexOf("netmask"))).trim();
+					} else {
+						ip = linha.trim();
 					}
 				}
-				if(adapter != "" && ip!= "") {
-					message += adapter + "\n" + ip + "\n" ;
+				if (adapter != "" && ip != "") {
+					message += adapter + "\n" + ip + "\n";
 					ip = "";
-			
+
 				}
 				linha = buffer.readLine();
 			}
 			buffer.close();
 			leitor.close();
-			fluxo.close();		
+			fluxo.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	
-    	return message;
+
+		return message;
 	}
-	
+
 	public void ip(String os) {
 
-		if(os.contains("Windows")) {
+		if (os.contains("Windows")) {
 			String process = "ipconfig";
-			String adapter = "";
-			String ip = "";
-			
+
 			System.out.println(callProcess(process));
-			
-			
-		}else if(os.contains("Linux")) {
+
+		} else if (os.contains("Linux")) {
 			String process = "ifconfig";
 
-			
 			System.out.println(callProcess(process));
-			
-			
-		}else if(os.contains("Mac")){
 
-			
-			
-			
-			
-		}else {
+		} else if (os.contains("Mac")) {
+			String process = "ifconfig";
+
+			System.out.println(callProcess(process));
+
+		} else {
 			System.out.println("SO não reconhecido");
 		}
-		
+
 	}
-	
+
 	public void ping() {
-		
+
 	}
 }
