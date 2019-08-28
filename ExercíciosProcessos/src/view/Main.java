0b126d02af4a6fa67a8063b55f82b850bf414214
Roney Cacau <roneycacau@gmail.com>
2019-08-28 13:51:29 -0300

    adicionando menu

--- ExercíciosProcessos/src/view/Main.java
+++ ExercíciosProcessos/src/view/Main.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Main {
@@ -7,10 +9,24 @@ public class Main {
 	public static void main(String[] args) {
 		RedesController controller = new RedesController();
 		String os = controller.OperationalSystem();
+		int op = 0;
 		
 		System.out.println(os);
 		
-		controller.ip(os);
+		do {
+			op = Integer.parseInt(JOptionPane.showInputDialog("Escolhe ai!"));
+			switch(op) {
+			case 1:
+				controller.ip(os);
+				break;
+			case 9: 
+				System.out.println("Tchau o/");
+				break;
+			default:
+				System.out.println("ERROU!");
+			}
+			
+		}while(op!=9);
 	}
 
 }
