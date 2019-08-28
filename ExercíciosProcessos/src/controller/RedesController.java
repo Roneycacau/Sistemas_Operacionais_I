47b0e35e2ded96c78f6c27c725dc81e85c01d833
Roney Cacau <roneycacau@gmail.com>
2019-08-28 13:01:46 -0300

    Melhorado legibilidade do código

--- ExercíciosProcessos/src/controller/RedesController.java
+++ ExercíciosProcessos/src/controller/RedesController.java
@@ -6,85 +6,76 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class RedesController {
-	
+
 	public String OperationalSystem() {
 		String os = System.getProperty("os.name");
 		return os;
 	}
-	
+
 	public String callProcess(String process) {
-		String message="";
+		String message = "";
 		String adapter = "";
 		String ip = "";
-    	try {
+		try {
 			Process p = Runtime.getRuntime().exec(process);
 			InputStream flow = p.getInputStream();
 			InputStreamReader reader = new InputStreamReader(flow);
 			BufferedReader buffer = new BufferedReader(reader);
 			String row = buffer.readLine();
-			while(row != null ) {
-				
-				if(row.contains("Adaptador")||row.contains("en")||row.contains("en")||row.contains("wl")) {
-					if(row.contains("en")||row.contains("en")||row.contains("wl")) {
-						adapter = row.substring(0, (row.indexOf(":")-1 	));
-					}else {
+			while (row != null) {
+
+				if (row.contains("enp") || row.contains("wlp")) {
+					adapter = row.substring(0, (row.indexOf(":")));
+				} else if(row.contains("Adaptador")){
 					adapter = row;
-					}
 				}
-				if(row.contains("IPv4")||row.contains("inet ")) {
-					if(row.contains("inet ")) {
-						adapter = row.substring(0, (row.lastIndexOf("netmask")));
-					}else{
-						ip = row;
-					}
+
+				if (row.contains("inet ")) {
+					ip = row.substring(0, (row.lastIndexOf(" netmask")));
+				} else if(row.contains("IPv4")){
+					ip = row;
 				}
-				if(adapter != "" && ip!= "") {
-					message += adapter + "\n" + ip + "\n" ;
+
+				if (adapter != "" && ip != "") {
+					message += adapter.trim() + ": " + ip.trim() + "\n";
 					ip = "";
-			
+
 				}
 				row = buffer.readLine();
 			}
 			buffer.close();
 			reader.close();
-			flow.close();		
+			flow.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	
-    	return message;
+
+		return message;
 	}
-	
+
 	public void ip(String os) {
 
-		if(os.contains("Windows")) {
+		if (os.contains("Windows")) {
 			String process = "ipconfig";
 			String adapter = "";
 			String ip = "";
-			
+
 			System.out.println(callProcess(process));
-			
-			
-		}else if(os.contains("Linux")) {
+
+		} else if (os.contains("Linux")) {
 			String process = "ifconfig";
 
-			
 			System.out.println(callProcess(process));
-			
-			
-		}else if(os.contains("Mac")){
 
-			
-			
-			
-			
-		}else {
+		} else if (os.contains("Mac")) {
+
+		} else {
 			System.out.println("SO não reconhecido");
 		}
-		
+
 	}
-	
+
 	public void ping() {
-		
+
 	}
 }
